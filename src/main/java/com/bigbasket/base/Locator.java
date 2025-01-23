dae1a46b20226e9fe600858af54d59f7321aac18
ushabandgar <ubandgar@birdzi.com>
2025-01-23 15:06:28 +0530

    Added 3 test cases on 23jan

---
+++ src/main/java/com/bigbasket/base/Locator.java
@@ -0,0 +1,7 @@
+package com.bigbasket.base;
+
+public interface Locator {
+	String shopByCategoryMenu="css##button[id*='headlessui-menu-button-:R5bab6:']";
+	String categoryList="xpath##//li[@class=\"jsx-1259984711\" and @role=\"none\"]";
+	String shopByCatgeorySubCategories="css##a[class=\"FilterByCategory___StyledLink3-sc-c0pkxv-4 bqthFs\"]";
+}
