a9e20d9fdfdbfdc6651a4497909e1fdde4d6fa47
unknown <varshasirsat849@gmail.com>
2025-01-26 20:52:55 -0600

    todays my 4 test case

--- src/main/java/com/bigbasket/stepdefinations/SelectLocationSteps.java
+++ src/main/java/com/bigbasket/stepdefinations/SelectLocationSteps.java
@@ -1,6 +1,7 @@
 package com.bigbasket.stepdefinations;
 
 import com.bigbasket.base.Keyword;
+import com.bigbasket.pages.HomePage;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
@@ -8,30 +9,65 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class SelectLocationSteps {
-	@Given("Browser launched")
-	public void browserLaunchedAndUrlIsLoadedSuccessfully() {
-		Keyword keyword = new Keyword();
-		keyword.openBrowser("Firefox");
 
-	}
-
-	@And("close browser")
-	public void closeBrowser() {
-		Keyword.driver.close();
-
-	}
-
-	@When("After enter url, User on HomePage")
-	public void userOnHomePageOrNot() {
-		Keyword keyword = new Keyword();
-		keyword.launchUrl("https://www.bigbasket.com/");
-	}
-	
 	@Then("{string} bar should be display on HomePage")
 	public void bar_should_be_display_on_home_page(String SelectLocation) {
-	    
+		HomePage homepage = new HomePage();
+		homepage.verifySelectLoactionBarVisibleOnTheHomepageOrNot();
+
 	}
 
+	@When("User click on select location bar")
+	public void user_click_on_select_location_bar() throws InterruptedException {
+		HomePage homepage = new HomePage();
+		homepage.clickonSelectLocationbar();
 
+	}
+
+	@When("User enter {string} into Search For Area TextBox")
+	public void user_enter_into_search_for_area_text_box(String string) throws InterruptedException {
+		HomePage homepage = new HomePage();
+		homepage.enterTextIntoSearchForAreaTextBox();
+
+	}
+
+	@Then("verify User can able to enter text into Search For Area TextBox")
+	public void verify_user_can_able_to_enter_text_into_search_for_area_text_box() {
+		HomePage homepage = new HomePage();
+		homepage.verifyThatUserCanAbleToTypeTextIntosearchForAreaTextBox();
+
+	}
+
+	@And("{string} placeholder text is appear in text box")
+	public void placeholder_text_is_appear_in_text_box(String SearchForArea) {
+		HomePage homepage = new HomePage();
+	}
+
+	@And("User enter {string} into TextBox")
+	public void user_enter_text_box(String Pune) {
+		HomePage homepage = new HomePage();
+		homepage.getValueOfAttributeBeforeAndAfterEntertext();
+
+	}
+
+	@Then("placeholder text should not be appear in text box")
+	public void placeholder_text_should_not_be_appear_in_text_box() {
+		HomePage homepage = new HomePage();
+		homepage.verifyThePlaceholderTextIsDisplayedWhenTheSeachForAreaTextBoxIsEmpty();
+
+	}
+
+	@And("User get list of address locations")
+	public void usergetListOfAddressOfLocations() {
+		HomePage homepage = new HomePage();
+		homepage.addressOfLoactions();
+	}
+
+	@Then("Address of location results should be appear which contains {string}")
+	public void address_of_location_results_should_be_appear_which_contains(String Pune) {
+		HomePage homepage = new HomePage();
+		homepage.verifythatAddressOfLocationResultsAreDisplayedWhenValidTextIsEnterIntoTextBOx();
+
+	}
 
 }
--- src/test/resources/Features/SelectLocation.feature
+++ src/test/resources/Features/SelectLocation.feature
@@ -1,6 +1,26 @@
 Feature: This feature file test Select Location functionality
 
- Scenario: Verify that "Select Location" bar is visible on the Homepage
- Given Browser launched
- When After enter url, User on HomePage
- Then "Select Location" bar should be display on HomePage
+  Scenario: Verify that "Select Location" bar is visible on the Homepage
+    Given Browser launched
+    When After enter url, User on HomePage
+    Then "Select Location" bar should be display on HomePage
+
+  Scenario: verify user can able to type text Into the "Select Location" bar
+    Given After enter url, User on HomePage
+    When User click on select location bar
+    And User enter "Pune" into Search For Area TextBox
+    Then verify User can able to enter text into Search For Area TextBox
+
+  Scenario: Verify the placeholder text "Search for area or street name" is displayed when the select loaction bar is empty
+    Given After enter url, User on HomePage
+    When User click on select location bar
+    And "Search for area or street name" placeholder text is appear in text box
+    And User enter "Pune" into TextBox
+    Then placeholder text should not be appear in text box
+
+  Scenario: Verify that  address location results are displayed when Enter "Pune"
+    Given After enter url, User on HomePage
+    When User click on select location bar
+    And User enter "Pune" into TextBox
+    And User get list of address locations
+    Then Address of location results should be appear which contains "Pune"
