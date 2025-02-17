c67c38e274890015e404a88692aa3508afc9da92
unknown <varshasirsat849@gmail.com>
2025-02-17 23:52:18 +0530

    add proprtities file and dyanamic browser and url in Hooks1

---
+++ src/main/java/com/propUtils/App.java
@@ -0,0 +1,19 @@
+package com.propUtils;
+
+import java.io.IOException;
+
+public class App {
+	
+	private static final String filepath="/src/main/resources/App.properties";
+	
+	public static  String getBrowserName() throws IOException{
+		String base=System.getProperty("user.dir");	
+		return PropertiesUtils.getProperty(base+filepath,"browser_name");
+	}
+	
+	public static  String getUrl(String env) throws IOException{
+		String base=System.getProperty("user.dir");	
+		return PropertiesUtils.getProperty(base+filepath,env+".url");
+	}
+
+}
---
+++ src/main/java/com/propUtils/PropertiesUtils.java
@@ -0,0 +1,30 @@
+package com.propUtils;
+
+
+	
+	import java.io.FileInputStream;
+	import java.io.FileNotFoundException;
+	import java.io.IOException;
+	import java.util.Properties;
+
+	public class PropertiesUtils {
+		
+		
+		public static String getProperty(String filepath,String key) throws IOException {
+			FileInputStream fis;
+			String base=System.getProperty("user.dir");//THIS IS SYSTEM PROPERTIES
+			System.out.println(base);//C:\Users\LENOVO\eclipse-workspace\frameworkJarvishMavenProject
+			fis =new FileInputStream(filepath);
+			
+			Properties prop=new Properties();
+			prop.load(fis);
+			String value=prop.getProperty(key);
+			return value;
+		
+			
+		}
+
+	}
+
+
+
